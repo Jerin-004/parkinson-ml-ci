8b2fc93b6dd1ac0ba70f8d8b27841a10fa788f79
Chijith Jerin Stalin <88652930+Jerin-004@users.noreply.github.com>
2025-07-25 11:22:16 +0530

    Updated commit

---
+++ tests/test_model.py
@@ -0,0 +1,18 @@
+import joblib
+import pandas as pd
+
+def test_model_prediction():
+    df = pd.read_csv("../data/parkinson_disease.csv")
+
+    if 'id' in df.columns:
+        df = df.drop('id', axis=1)
+
+    if 'gender' in df.columns and df['gender'].dtype == object:
+        df = pd.get_dummies(df, columns=['gender'], drop_first=True)
+
+    X = df.drop("class", axis=1)
+
+    model = joblib.load("model.pkl")
+    preds = model.predict(X)
+
+    assert len(preds) == len(X)
